3647a62db359e9bcfc55c62fef8a5955e0a4bd86
adithya-coder-dev <adithya10100@gmail.com>
2025-12-20 09:37:11 +0530

    DAY 5

--- controller/models.py
+++ controller/models.py
@@ -2,7 +2,7 @@ from controller.database import db
 
 class User(db.Model):
     __tablename__ = 'users'
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
@@ -19,16 +19,19 @@ class Role(db.Model):
 class UserRole(db.Model):
     __tablename__ = 'user_role'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.role_id'))
 
 class Staff(db.Model):
     __tablename__ = 'staff'
     staff_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    department = db.Column(db.String(100), nullable=False)
 
 class Student(db.Model):
     __tablename__ = 'student'
     student_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
-   
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    enrollment_year = db.Column(db.Integer, nullable=False)
+    department = db.Column(db.String(100), nullable=False)
+    flag = db.Column(db.Boolean, default=True)
